c329f588f4c2877246019046725fa585a3f9439b
Ken <kenin.qiu@gmail.com>
2025-05-03 18:14:14 -0400

    update

--- resources/views/blocks/code.blade.php
+++ resources/views/blocks/code.blade.php
@@ -1,1 +1,1 @@
-<pre class="bg-gray-100 text-sm text-gray-800 rounded-lg p-4 overflow-x-auto"><code class="whitespace-pre-line font-mono">{{ trim($content) }}</code></pre>
+<pre class="bg-gray-100 text-sm text-gray-800 rounded-lg p-4 overflow-x-auto"><code class="whitespace-pre-line font-mono">{{ $content }}</code></pre>
