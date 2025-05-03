f125d959a465d3e8d927eb3759754186cbab5acf
Ken <kenin.qiu@gmail.com>
2025-05-03 18:21:05 -0400

    update

--- resources/views/blocks/code.blade.php
+++ resources/views/blocks/code.blade.php
@@ -1,11 +1,1 @@
-<x-code-block :content="
-    'contract Token {
-        mapping(address => uint256) balances;
-
-        function transfer(address to, uint256 amount) public {
-            require(balances[msg.sender] >= amount);
-            balances[msg.sender] -= amount;
-            balances[to] += amount;
-        }
-    }'
-" />
+<x-code-block :content="$content" />
