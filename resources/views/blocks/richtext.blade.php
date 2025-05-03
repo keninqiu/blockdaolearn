416f030e71e502e806e92ba3fb9da50d20c4c84b
Ken <kenin.qiu@gmail.com>
2025-05-03 14:49:09 -0400

    update

--- resources/views/blocks/richtext.blade.php
+++ resources/views/blocks/richtext.blade.php
@@ -2,19 +2,36 @@
 $html = $content;
 
 
-// 处理 <ol>
-$html = preg_replace(
-    '/<ol(.*?)class="(.*?)"(.*?)>/i',
-    '<ol$1class="$2 ol-item"$3>',
-    preg_replace('/<ol(?![^>]*class)/i', '<ol class="ol-item"', $html)
-);
+// Define the classes for each element
+$classes = [
+    'ol' => ['ol-item'],
+    'ul' => ['list-disc', 'pl-8'],
+    'table' => ['w-full', 'border-collapse', 'border', 'border-gray-300'],
+    'th' => ['border', 'border-gray-300', 'p-2'],
+    'td' => ['border', 'border-gray-300', 'p-2']
+];
 
-// 处理 <ul>
-$html = preg_replace(
-    '/<ul(.*?)class="(.*?)"(.*?)>/i',
-    '<ul$1class="$2 list-disc pl-8"$3>',
-    preg_replace('/<ul(?![^>]*class)/i', '<ul class="list-disc pl-8"', $html)
-);
+
+// Process each element type (ol, ul, table) and add the classes
+foreach ($classes as $element => $elementClasses) {
+    // Combine the classes into a single string
+    $classAttr = implode(' ', $elementClasses);
+
+    // Regex to add class to elements that already have a class attribute
+    $html = preg_replace_callback(
+        '/<' . $element . '(.*?)class="(.*?)"(.*?)>/i',
+        function ($matches) use ($element, $classAttr) {
+            // Append the new class to existing ones
+            return "<{$element}{$matches[1]}class=\"{$matches[2]} {$classAttr}\"{$matches[3]}>";
+        },
+        // Regex to add class to elements that do not have a class attribute
+        preg_replace(
+            '/<' . $element . '(?![^>]*class)/i',
+            "<$element class=\"$classAttr\"",
+            $html
+        )
+    );
+}
 
 @endphp
 <div class="prose max-w-none ol-reset">
