4d8a15d85bb11046cb389a19a4411e2785dc1eb3
Ken <kenin.qiu@gmail.com>
2025-05-05 13:17:55 -0400

    update

---
+++ chrome/tutorial-extension/steps/rainbowwallet/step7.js
@@ -0,0 +1,6 @@
+var steps = [
+    { delay: 3200,  description: "Rainbow支持多钱包" },
+    { delay: 3200,  description: "点击钱包地址" },
+    { delay: 3200,  description: "选择添加其他钱包" },
+    { delay: 3200,  description: "或者从硬件钱包连接" },
+];
