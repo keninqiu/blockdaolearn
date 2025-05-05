6c5d3e334bad6ac2b39fa6d7eb2409c7e1803ddf
Ken <kenin.qiu@gmail.com>
2025-05-05 06:57:34 -0400

    f

---
+++ chrome/tutorial-extension/steps/rainbowwallet.js
@@ -0,0 +1,31 @@
+var steps = [
+    { delay: 2000,  description: "大家好，我叫Ken" },
+    { delay: 2500,  description: "又到了区块链学习的时间" },
+    { delay: 3000,  description: "今天我们来认识一款非常热门的钱包" },
+    { delay: 4000,  description: "Rainbow 钱包" },
+    { delay: 6000,  description: "简单、安全又好用" },
+    { delay: 3200,  description: "适合所有链上新手和老玩家！" },
+    { delay: 3200,  description: "Rainbow 钱包虽然不主打多链" },
+    { delay: 3200,  description: "但它把以太坊、Layer2（Arbitrum、Optimism等）的体验做得非常好" },
+    { delay: 3200,  description: "而且它还有一套积分系统" },
+    { delay: 3200,  description: "Rainbow 官方早期有过一些融资" },
+    { delay: 3200,  description: "像 2022 年拿到了 Union Square Ventures、Seven Seven Six 等机构投资" },
+    { delay: 3200,  description: "所以社区里有很多人在猜测未来可能会发币" },
+    { delay: 3200,  description: "如果发币并且有空投的话" },
+    { delay: 3200,  description: "那积分应该是空投很重要的参考指标" },
+
+    { delay: 3200,  description: "首先我们打开rainbow钱包的官网" },
+    { delay: 3200,  description: "rainbow.me" },
+    { delay: 3200,  description: "点击Download进入下载页面" },
+    { delay: 3200,  description: "有两大类版本" },
+    { delay: 3200,  description: "包括桌面版和手机版" },
+    { delay: 3200,  description: "桌面版支持六大主流的浏览器" },
+    { delay: 3200,  description: "手机版分为IOS和Android版" },
+    { delay: 3200,  description: "本期视频重点讲解Chrome浏览器插件版本" },
+
+    { delay: 3200,  description: "点击Chrome，再点击Add to Chrome，Add extension" },
+    { delay: 3200,  description: "Chrome浏览器插件钱包安装完毕" },
+    
+]
+
+
